bcefd23860124a3790ebe68650c1267429b687dd
Gil <gilgru54@gmail.com>
2020-10-22 15:35:39 +0300

    Updates

---
+++ precache-manifest.e24f04306d30727cdd336e34ccfc62c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa81061e86d083a4de1761933836549",
+    "url": "/ReactProj/index.html"
+  },
+  {
+    "revision": "b8e2496477aeddf7a5dd",
+    "url": "/ReactProj/static/js/2.a8958272.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/ReactProj/static/js/2.a8958272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1dbf9dcb73b79f8194",
+    "url": "/ReactProj/static/js/main.e4a1b2e7.chunk.js"
+  },
+  {
+    "revision": "ceda2548a5cff87fcb3e",
+    "url": "/ReactProj/static/js/runtime-main.2cab6620.js"
+  }
+]);
